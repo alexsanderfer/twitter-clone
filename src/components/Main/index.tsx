4689a8188171b23b4a0ce37743ee1ec193a0fc04
Alexsander <33939423+alexsanderfer@users.noreply.github.com>
2020-11-09 10:09:37 +0000

    Add files via upload

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import ProfilePage from '../ProfilePage';
+
+import {
+  Container,
+  Header,
+  BackIcon,
+  ProfileInfo,
+  BottomMenu,
+  HomeIcon,
+  SearchIcon,
+  BellIcon,
+  EmailIcon
+} from './style';
+
+const Main: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <button>
+          <BackIcon/>
+        </button>
+        <ProfileInfo>
+          <strong>Alexsander</strong>
+          <span>999 Tweets</span>
+        </ProfileInfo>
+      </Header>
+
+      <ProfilePage/>
+
+      {<BottomMenu>
+        <HomeIcon className="active" />
+        <SearchIcon/>
+        <BellIcon/>
+        <EmailIcon/>
+      </BottomMenu>}
+    </Container>
+  )
+}
+
+export default Main;
